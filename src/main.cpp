32edc19c6a1f7e6efaf59daaa609738e53ae0fb6
ryushkov <r.yuschkoff@mail.ru>
2022-11-29 15:14:06 +0300

    git push

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,49 @@
+
+/*******************************************************************************
+ * LVGL Widgets
+ * This is a widgets demo for LVGL - Light and Versatile Graphics Library
+ * import from: https://github.com/lvgl/lv_demos.git
+ *
+ * Dependent libraries:
+ * LVGL: https://github.com/lvgl/lvgl.git
+
+ * Touch libraries:
+ * FT6X36: https://github.com/strange-v/FT6X36.git
+ * GT911: https://github.com/TAMCTec/gt911-arduino.git
+ * XPT2046: https://github.com/PaulStoffregen/XPT2046_Touchscreen.git
+ *
+ * LVGL Configuration file:
+ * Copy your_arduino_path/libraries/lvgl/lv_conf_template.h
+ * to your_arduino_path/libraries/lv_conf.h
+ * Then find and set:
+ * #define LV_COLOR_DEPTH     16
+ * #define LV_TICK_CUSTOM     1
+ *
+ * For SPI display set color swap can be faster, parallel screen don't set!
+ * #define LV_COLOR_16_SWAP   1
+ *
+ * Optional: Show CPU usage and FPS count
+ * #define LV_USE_PERF_MONITOR 1
+ ******************************************************************************/
+//#include "lv_demo_widgets.h"
+#include <lvgl.h>
+//#include <demos/lv_demos.h>
+/*******************************************************************************
+ ******************************************************************************/
 #include <Arduino_GFX_Library.h>
-#include <TAMC_GT911.h>
-
-
-
-#define CS_PIN  38
-#define GFX_BL DF_GFX_BL
 #define TFT_BL 2
-#define TOUCH_SDA  19
-#define TOUCH_SCL  20
-#define TOUCH_INT 18
-#define TOUCH_RST 38
-#define TOUCH_WIDTH  480
-#define TOUCH_HEIGHT 272
-TAMC_GT911 tp = TAMC_GT911(TOUCH_SDA, TOUCH_SCL, TOUCH_INT, TOUCH_RST, TOUCH_WIDTH, TOUCH_HEIGHT);
+#define GFX_BL DF_GFX_BL // default backlight pin, you may replace DF_GFX_BL to actual backlight pin
 
+/* More dev device declaration: https://github.com/moononournation/Arduino_GFX/wiki/Dev-Device-Declaration */
 #if defined(DISPLAY_DEV_KIT)
 Arduino_GFX *gfx = create_default_Arduino_GFX();
-#else
+#else /* !defined(DISPLAY_DEV_KIT) */
 
+/* More data bus class: https://github.com/moononournation/Arduino_GFX/wiki/Data-Bus-Class */
+//Arduino_DataBus *bus = create_default_Arduino_DataBus();
+
+/* More display class: https://github.com/moononournation/Arduino_GFX/wiki/Display-Class */
+//Arduino_GFX *gfx = new Arduino_ILI9341(bus, DF_GFX_RST, 0 /* rotation */, false /* IPS */);
 
 Arduino_ESP32RGBPanel *bus = new Arduino_ESP32RGBPanel(
     GFX_NOT_DEFINED /* CS */, GFX_NOT_DEFINED /* SCK */, GFX_NOT_DEFINED /* SDA */,
@@ -26,57 +52,180 @@ Arduino_ESP32RGBPanel *bus = new Arduino_ESP32RGBPanel(
     5 /* G0 */, 6 /* G1 */, 7 /* G2 */, 15 /* G3 */, 16 /* G4 */, 4 /* G5 */,
     8 /* B0 */, 3 /* B1 */, 46 /* B2 */, 9 /* B3 */, 1 /* B4 */
 );
-
+// option 1:
+// ST7262 IPS LCD 800x480
  Arduino_RPi_DPI_RGBPanel *gfx = new Arduino_RPi_DPI_RGBPanel(
    bus,
    800 /* width */, 0 /* hsync_polarity */, 8 /* hsync_front_porch */, 4 /* hsync_pulse_width */, 8 /* hsync_back_porch */,
    480 /* height */, 0 /* vsync_polarity */, 8 /* vsync_front_porch */, 4 /* vsync_pulse_width */, 8 /* vsync_back_porch */,
-   1 /* pclk_active_neg */, 16000000 /* prefer_speed */, true /* auto_flush */);
+   1 /* pclk_active_neg */, 14000000 /* prefer_speed */, true /* auto_flush */);
+#endif /* !defined(DISPLAY_DEV_KIT) */
+/*******************************************************************************
+ * End of Arduino_GFX setting
+ ******************************************************************************/
 
-#endif 
+/*******************************************************************************
+ * Please config the touch panel in touch.h
+ ******************************************************************************/
+#include "touch.h"
+
+//добавлено для примера создания экрана
+static lv_obj_t * scr;
+static lv_obj_t * arc;
+static lv_obj_t * label;
 
 
-void setup(void)
+/* Change to your screen resolution */
+static uint32_t screenWidth;
+static uint32_t screenHeight;
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t *disp_draw_buf;
+static lv_disp_drv_t disp_drv;
+
+/* Display flushing */
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
 {
-    gfx->begin();
-    tp.begin();
-    tp.setRotation(ROTATION_INVERTED);
-    delay(1000);
-    gfx->fillScreen(BLACK);
-    delay(1000);
-    gfx->fillRoundRect(40,30,200,160,6,YELLOW);
-    gfx->fillRoundRect(290,30,200,160,6,YELLOW);
-    gfx->fillRoundRect(540,30,200,160,6,YELLOW);
-    gfx->fillRoundRect(40,220,200,160,6,YELLOW);
-    gfx->fillRoundRect(290,220,200,160,6,YELLOW);
-    gfx->fillRoundRect(540,220,200,160,6,YELLOW);
-#ifdef TFT_BL
-    pinMode(TFT_BL, OUTPUT);
-    digitalWrite(TFT_BL, HIGH);
+  uint32_t w = (area->x2 - area->x1 + 1);
+  uint32_t h = (area->y2 - area->y1 + 1);
+
+#if (LV_COLOR_16_SWAP != 0)
+  gfx->draw16bitBeRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#else
+  gfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
 #endif
-    delay(1000); 
+
+  lv_disp_flush_ready(disp);
+}
+
+void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
+{
+  if (touch_has_signal())
+  {
+    if (touch_touched())
+    {
+      data->state = LV_INDEV_STATE_PR;
+
+      /*Set the coordinates*/
+      data->point.x = touch_last_x;
+      data->point.y = touch_last_y;
+    }
+    else if (touch_released())
+    {
+      data->state = LV_INDEV_STATE_REL;
+    }
+  }
+  else
+  {
+    data->state = LV_INDEV_STATE_REL;
+  }
+}
+
+void arc_cb(lv_event_t * e) {
+  uint32_t val = lv_arc_get_value(arc);
+    char buf[10];
+    sprintf(buf, "val = %d", val);
+    lv_label_set_text(label, buf);
+}
+
+void create_screen() {
+  lv_theme_default_init(NULL, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED), LV_THEME_DEFAULT_DARK,
+                          LV_FONT_DEFAULT);
+
+  scr = lv_obj_create(NULL);
+
+    label = lv_label_create(scr);
+    lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR);     /*Circular scroll*/
+    lv_obj_set_width(label, 150);
+    lv_label_set_text(label, "It is a circularly scrolling text. ");
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, -140);
+
+
+    arc = lv_arc_create(scr);
+  lv_obj_set_size(arc, 150, 150);
+  lv_arc_set_rotation(arc, 135);
+  lv_arc_set_bg_angles(arc, 0, 270);
+  lv_arc_set_value(arc, 40);
+  lv_obj_center(arc);
+  lv_obj_align(arc, LV_ALIGN_CENTER, 0, 140);
+  lv_obj_add_event_cb(arc, arc_cb, LV_EVENT_ALL, NULL);
+
+  lv_scr_load(scr);
 }
 
 
+
+
+void setup()
+{
+  Serial.begin(115200);
+  // while (!Serial);
+  Serial.println("LVGL Widgets Demo");
+
+  // Init touch device
+  
+
+  // Init Display
+  gfx->begin();
+#ifdef TFT_BL
+  pinMode(TFT_BL, OUTPUT);
+  digitalWrite(TFT_BL, HIGH);
+#endif
+  
+  lv_init();
+  delay(10);
+  touch_init();
+  screenWidth = gfx->width();
+  screenHeight = gfx->height();
+#ifdef ESP32
+  disp_draw_buf = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * screenWidth * screenHeight/4 , MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+#else
+  disp_draw_buf = (lv_color_t *)malloc(sizeof(lv_color_t) * screenWidth * screenHeight/4);
+#endif
+  if (!disp_draw_buf)
+  {
+    Serial.println("LVGL disp_draw_buf allocate failed!");
+  }
+  else
+  {
+    lv_disp_draw_buf_init(&draw_buf, disp_draw_buf, NULL, screenWidth * screenHeight/4);
+
+    /* Initialize the display */
+    lv_disp_drv_init(&disp_drv);
+    /* Change the following line to your display resolution */
+    disp_drv.hor_res = screenWidth;
+    disp_drv.ver_res = screenHeight;
+    disp_drv.flush_cb = my_disp_flush;
+    disp_drv.draw_buf = &draw_buf;
+    lv_disp_drv_register(&disp_drv);
+
+    /* Initialize the (dummy) input device driver */
+    static lv_indev_drv_t indev_drv;
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    indev_drv.read_cb = my_touchpad_read;
+    lv_indev_drv_register(&indev_drv);
+
+    //lv_demo_widgets();
+
+    create_screen();
+
+    Serial.println("Setup done");
+  }
+}
+
 void loop()
 {
-	tp.read();
-	if (tp.isTouched){
-		for (int i=0; i<tp.touches; i++){
-            int x = (tp.points[i].x);
-            int y = (tp.points[i].y);
-			if (x>26 && x<140 && y>26 && y<105){gfx->fillRoundRect(40,30,200,160,6,GREEN);}
-			if (x>184 && x<289 && y>26 && y<105){gfx->fillRoundRect(290,30,200,160,6,GREEN);}
-			if (x>328 && x<439 && y>26 && y<105){gfx->fillRoundRect(540,30,200,160,6,GREEN);}
-			if (x>24 && x<145 && y>130 && y<210){gfx->fillRoundRect(40,220,200,160,6,GREEN);}
-			if (x>179 && x<294 && y>130 && y<210){gfx->fillRoundRect(290,220,200,160,6,GREEN);}
-			if (x>332 && x<444 && y>130 && y<210){gfx->fillRoundRect(540,220,200,160,6,GREEN);}
-			gfx->setTextSize(2);
-			gfx->setCursor(0, 0);
-			gfx->print("  x: ");gfx->print(x);
-			gfx->print("  y: ");gfx->print(y);
-			delay(3000);
-			gfx->fillRect(0,0,200,20,BLACK);
-		}
-	}
+  lv_timer_handler(); /* let the GUI do its work */
+  delay(5);
+
+  static uint32_t ms;
+  /*if (millis() - ms > 1000) {
+    ms = millis();
+    uint32_t val = lv_arc_get_value(arc);
+    char buf[10];
+    sprintf(buf, "val = %d", val);
+    lv_label_set_text(label, buf);
+  }*/
+
+
 }
