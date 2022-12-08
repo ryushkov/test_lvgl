dbc33760dc00341eab7accbea856cb83e29b545d
ryushkov <r.yuschkoff@mail.ru>
2022-12-08 16:48:45 +0300

    git push

--- src/main1.cpp
+++ src/main1.cpp
@@ -22,6 +22,7 @@ Arduino_RPi_DPI_RGBPanel *gfx = new Arduino_RPi_DPI_RGBPanel(
 #include "touch.h"
 
 //добавлено для примера создания экрана
+static lv_obj_t * kb;
 
 /* Change to your screen resolution */
 static uint32_t screenWidth;
@@ -29,8 +30,10 @@ static uint32_t screenHeight;
 static lv_disp_draw_buf_t draw_buf;
 static lv_color_t *disp_draw_buf;
 static lv_disp_drv_t disp_drv;
+//обьявление функций
+static void ta_event_cb(lv_event_t * e);
 
-/* Display flushing */
+//инициализация дисплея
 void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
 {
   uint32_t w = (area->x2 - area->x1 + 1);
@@ -45,6 +48,7 @@ void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color
   lv_disp_flush_ready(disp);
 }
 
+//инициализация тачскрина
 void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
 {
   if (touch_has_signal())
@@ -67,51 +71,67 @@ void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
     data->state = LV_INDEV_STATE_REL;
   }
 }
-static void event_handler(lv_event_t *e);
-void first_screen()
-{
-  lv_theme_default_init(NULL, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED), LV_THEME_DEFAULT_DARK,
-                        LV_FONT_DEFAULT);
-  
-  lv_obj_t *scr1 = lv_obj_create(NULL);
-  lv_obj_t *btn1 = lv_btn_create(scr1);
-  lv_obj_t *label = lv_label_create(btn1);
-  lv_obj_align(btn1, LV_ALIGN_CENTER, 0, -80);
-  lv_label_set_text(label, "keyboard");
-  lv_obj_add_event_cb(btn1, event_handler, LV_EVENT_ALL, NULL);
 
-  lv_scr_load(scr1);
+void lv_example_textarea_2(void)
+{
+    /*Create the password box*/
+    lv_obj_t * pwd_ta = lv_textarea_create(lv_scr_act());
+    lv_textarea_set_text(pwd_ta, "");
+    lv_textarea_set_password_mode(pwd_ta, true);
+    lv_textarea_set_one_line(pwd_ta, true);
+    lv_obj_set_width(pwd_ta, lv_pct(40));
+    lv_obj_set_pos(pwd_ta, 5, 20);
+    lv_obj_add_event_cb(pwd_ta, ta_event_cb, LV_EVENT_ALL, NULL);
+
+    /*Create a label and position it above the text box*/
+    lv_obj_t * pwd_label = lv_label_create(lv_scr_act());
+    lv_label_set_text(pwd_label, "Password:");
+    lv_obj_align_to(pwd_label, pwd_ta, LV_ALIGN_OUT_TOP_LEFT, 0, 0);
+
+    /*Create the one-line mode text area*/
+    lv_obj_t * text_ta = lv_textarea_create(lv_scr_act());
+    lv_textarea_set_one_line(text_ta, true);
+    lv_textarea_set_password_mode(text_ta, false);
+    lv_obj_set_width(text_ta, lv_pct(40));
+    lv_obj_add_event_cb(text_ta, ta_event_cb, LV_EVENT_ALL, NULL);
+    lv_obj_align(text_ta, LV_ALIGN_TOP_RIGHT, -5, 20);
+
+
+    /*Create a label and position it above the text box*/
+    lv_obj_t * oneline_label = lv_label_create(lv_scr_act());
+    lv_label_set_text(oneline_label, "Text:");
+    lv_obj_align_to(oneline_label, text_ta, LV_ALIGN_OUT_TOP_LEFT, 0, 0);
+
+    /*Create a keyboard*/
+    kb = lv_keyboard_create(lv_scr_act());
+    lv_obj_set_size(kb,  LV_HOR_RES, LV_VER_RES / 2);
+
+    lv_keyboard_set_textarea(kb, pwd_ta); /*Focus it on one of the text areas to start*/
 }
 
-void kbd_screen()
-{
-  lv_theme_default_init(NULL, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED), LV_THEME_DEFAULT_DARK,
-                        LV_FONT_DEFAULT);
+void start_menu(){
+lv_theme_default_init(NULL, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED), LV_THEME_DEFAULT_DARK,
+  LV_FONT_DEFAULT);
 
-  lv_obj_t *scr = lv_obj_create(NULL);
-  lv_obj_t *kb = lv_keyboard_create(scr);
-  lv_obj_t *ta;
-  ta = lv_textarea_create(scr);
-  lv_obj_align(ta, LV_ALIGN_TOP_MID, 0, 20);
-  lv_obj_set_size(ta, 700, 180);
+  static const char * btnm_map[] = {"1", "2", "3", "4", "5", "\n",
+                                  "6", "7", "8", "9", "0", "\n",
+                                  "Action1", "Action2", ""};
 
-  lv_keyboard_set_textarea(kb, ta);
-  lv_obj_add_event_cb(kb, event_handler, LV_EVENT_READY, NULL);
-
-  lv_scr_load(scr);
+    lv_example_textarea_2();
+    
 }
-static void event_handler(lv_event_t *e)
-{
-  lv_event_code_t code = lv_event_get_code(e);
 
-  if (code == LV_EVENT_CLICKED)
-  {
-    kbd_screen();
-  }
-  if
-    (code ==LV_EVENT_READY)
-    {
-      first_screen();
+static void ta_event_cb(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t * ta = lv_event_get_target(e);
+    if(code == LV_EVENT_CLICKED || code == LV_EVENT_FOCUSED) {
+        /*Focus on the clicked text area*/
+        if(kb != NULL) lv_keyboard_set_textarea(kb, ta);
+    }
+
+    else if(code == LV_EVENT_READY) {
+        LV_LOG_USER("Ready, current text: %s", lv_textarea_get_text(ta));
     }
 }
 
@@ -158,7 +178,7 @@ void setup()
     indev_drv.read_cb = my_touchpad_read;
     lv_indev_drv_register(&indev_drv);
 
-    first_screen();
+    start_menu();
   }
 }
 
